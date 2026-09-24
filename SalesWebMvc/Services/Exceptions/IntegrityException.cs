f6c4dd640a64e2383eb73b3ccc513071e778f1b7
Bruno-AngeloBr <acer2005br@gmail.com>
2026-09-23 22:41:15 -0300

    Exception handling for delete (referencial integrity)

---
+++ SalesWebMvc/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace SalesWebMvc.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message)
+        {
+
+        }
+    }
+}
